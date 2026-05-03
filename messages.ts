015fefd60b2e704304a0e7753db65269d1aeefa5
cristian <cristian@Cristerminator.cristi.com>
2026-05-03 17:16:14 +0300

    updated a message(ca nu am tip )

--- messages.ts
+++ messages.ts
@@ -4,6 +4,7 @@ const images = [
     "https://res.cloudinary.com/dosnqe8ft/image/upload/v1777748839/ebe8a73353b3775cfdcb7b90eb76cb6b_kzqyov.png",
     "https://res.cloudinary.com/dosnqe8ft/image/upload/v1777754760/d9b06f693732052928f2f562167263cb_yeigwk.png",
     "https://res.cloudinary.com/dosnqe8ft/image/upload/v1777754803/c5d02ff66309903e7c2322daf0f76e1c_mavmea.png",
+    "https://res.cloudinary.com/djp9ziyn6/image/upload/v1777817162/4b3e90f7169b711ccc3219b8bc47871b_1_rdiqj6.jpg"
 ];
 
 export var cache: Map<string, Buffer> = new Map();
@@ -25,7 +26,10 @@ const messages: RandomMessage[] = [
     {
         allowedImages: [0],
         content: "ʕ⁠ ⁠ꈍ⁠ᴥ⁠ꈍ⁠ʔ",
-    }
+    },
+    {
+	allowedImages: [3],
+	content: "Adri are branza la sub brat",}
 ];
 
 export function getRandomImage() {
